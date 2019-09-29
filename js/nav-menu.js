9e21bbb810638533792403f012a90185ce9077eb
Igor <46229356+NekroMagus@users.noreply.github.com>
2019-09-29 23:21:33 +0300

    version 3.0

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -3,3 +3,13 @@ $('.menu-btn').on('click', function(e) {
     $('.menu').toggleClass('menu_active');
     $('.menu').toggleClass('content_active');
 });
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 70) {
+        $(".menu-btn").show();
+        $(".menu-btn2").hide();
+    } else {
+        $(".menu-btn2").show();
+        $(".menu-btn").hide();
+    }
+});
