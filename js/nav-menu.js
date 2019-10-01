4b55b3ccc2e3a2af63d09d616f8699d3a6f791d0
Igor <46229356+NekroMagus@users.noreply.github.com>
2019-10-01 09:54:34 +0300

    v3.06

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -9,6 +9,10 @@ $(window).scroll(function () {
         $(".menu-btn").show();
         $(".menu-btn2").hide();
     } else {
+        if (document.getElementById("imagine").scr==='img/bmw.png'){
+            alert(123);
+            return;
+        }
         $(".menu-btn2").show();
         $(".menu-btn").hide();
     }
@@ -22,7 +26,6 @@ let i = 0;
 let array = ['img/bm.png', 'img/bmw.png'];
 
 function changeNav() {
-
     if (i !== array.length - 1) {
         i++;
     } else {
@@ -30,3 +33,36 @@ function changeNav() {
     }
     document.getElementById('imagine').src = array[i];
 }
+
+
+$(window).scroll(function (){
+    $('.mov-right').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+450) {
+            $(this).addClass('fadeInRight');
+        }
+    });
+});
+
+
+$(window).scroll(function (){
+    $('.mov-left').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+450) {
+            $(this).addClass('fadeInLeft');
+        }
+    });
+});
+
+
+$(window).scroll(function (){
+    $('.mov-up').each(function (){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+450) {
+            $(this).addClass('fadeInUp  ');
+        }
+    });
+});
